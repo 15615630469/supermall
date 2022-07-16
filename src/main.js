24740a80cf04c1a4f363a6b3eb4130fa37c8041c
guohuamin <15615630469@163.com>
2022-07-16 20:31:39 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App).mount('#app')
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
+import toast from 'components/common/toast'
+
+createApp(App).config.productionTip = false
+
+// 添加事件总线对象
+createApp(App).prototype.$bus = new Vue()
+
+// 解决移动端300ms延迟
+FastClick.attach(document.body)
+
+// 图片懒加载
+createApp(App).use(VueLazyLoad, {
+  loading: require('./assets/img/common/placeholder.png')
+})
+
+createApp(App).use(router).use(store).use(toast).mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import { createApp } from "vue";
+import { createStore } from "vuex";
+
+import mutations from "./mutations";
+import actions from "./actions";
+import getters from "./getters";
+
+const state = {
+  cartList: []
+}
+
+const store = createStore({
+  state,
+  mutations,
+  actions,
+  getters
+})
+
+const app = createApp(App)
+
+app.use(store)
+
+export default store
